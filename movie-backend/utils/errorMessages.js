f7b11e88641f0f0809b19c1d6950312e3b5aa209
CurtMinyard <cminyard@perseverenow.org>
2025-09-26 09:45:43 -0500

    Initial Commit

--- movie-backend/utils/errorMessages.js
+++
@@ -1,8 +0,0 @@
-const errorMessages = {
-    firstName : 'firstName must be alphabetical',
-    lastName : 'lastName must be alphabetical',
-    userName : 'userName must be alphanumeric',
-    email : 'email must be valid' 
-}
-
-module.exports = { errorMessages };
