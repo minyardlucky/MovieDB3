08fe0ebcf1a7f3ad94cd0133d4e3424f1e27dd15
CurtMinyard <cminyard@perseverenow.org>
2025-10-31 15:19:37 -0500

    ready for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  //base:'/MovieDB3/',
-  base: '/', // ✅ local testing base
+  base:'/MovieDB3/',
   plugins: [react()],
 })
