4e13ad0a84824ecf8eef5802ec94143d322a2f6b
CurtMinyard <cminyard@perseverenow.org>
2025-10-31 15:33:45 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base:'/MovieDB3/',
+  base: '/MovieDB3/',  // 👈 must match your repo name exactly (case-sensitive)
   plugins: [react()],
 })
+
